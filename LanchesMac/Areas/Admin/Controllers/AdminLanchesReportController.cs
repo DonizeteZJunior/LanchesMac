21bfe6a8e2189e8404bc978e5fad9a589c3fbbf4
DonizeteZJunior <91087078+DonizeteZJunior@users.noreply.github.com>
2022-12-02 13:56:34 -0300

    Progresso Final curso. 02-12-2022.

---
+++ LanchesMac/Areas/Admin/Controllers/AdminLanchesReportController.cs
@@ -0,0 +1,67 @@
+﻿using FastReport.Data;
+using FastReport.Export.PdfSimple;
+using FastReport.Web;
+using LanchesMac.Areas.Admin.FastReportUtils;
+using LanchesMac.Areas.Admin.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize("Admin")]
+    public class AdminLanchesReportController : Controller
+    {
+        private readonly IWebHostEnvironment webHostEnv;
+        private readonly RelatorioLanchesService relatorioLanches;
+
+        public AdminLanchesReportController(IWebHostEnvironment _webHostEnv, RelatorioLanchesService _relatorioLanches)
+        {
+            webHostEnv = _webHostEnv;
+            relatorioLanches = _relatorioLanches;
+        }
+
+        public async Task<ActionResult> LanchesCategoriaReport()
+        {
+            var webReport = new WebReport();
+            var mssqlDataConnection = new MsSqlDataConnection();
+
+            webReport.Report.Dictionary.AddChild(mssqlDataConnection);
+            webReport.Report.Load(Path.Combine(webHostEnv.ContentRootPath,"wwwroot/reports",
+                                  "LanchesCategoria.frx"));
+
+            var lanches = HelperFastReport.GetTable(await relatorioLanches.GetLanchesReport(),"LanchesReport");
+            var categoria = HelperFastReport.GetTable(await relatorioLanches.GetCategoriaReport(), "CategoriaReport");
+
+            webReport.Report.RegisterData(lanches, "LancheReport");
+            webReport.Report.RegisterData(categoria, "CategoriaReport");
+            return View(webReport);
+        }
+
+        public async Task<ActionResult> LanchesCategoriaPDF()
+        {
+            var webReport = new WebReport();
+            var mssqlDataConnection = new MsSqlDataConnection();
+
+            webReport.Report.Dictionary.AddChild(mssqlDataConnection);
+            webReport.Report.Load(Path.Combine(webHostEnv.ContentRootPath, "wwwroot/reports",
+                                  "LanchesCategoria.frx"));
+
+            var lanches = HelperFastReport.GetTable(await relatorioLanches.GetLanchesReport(), "LanchesReport");
+            var categoria = HelperFastReport.GetTable(await relatorioLanches.GetCategoriaReport(), "CategoriaReport");
+
+            webReport.Report.RegisterData(lanches, "LancheReport");
+            webReport.Report.RegisterData(categoria, "CategoriaReport");
+
+            webReport.Report.Prepare();
+
+            Stream stream = new MemoryStream();
+
+            webReport.Report.Export(new PDFSimpleExport(), stream);
+            stream.Position = 0;
+
+            //return File(stream, "application/zip", "LancheCategoria.pdf");
+            return new FileStreamResult(stream, "application/pdf");
+        }
+    }
+}
---
+++ LanchesMac/Areas/Admin/Services/RelatorioLanchesService.cs
@@ -0,0 +1,36 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Areas.Admin.Services
+{
+    public class RelatorioLanchesService
+    {
+        private readonly AppDbContext context;
+
+        public RelatorioLanchesService(AppDbContext _context)
+        {
+            context = _context;
+        }
+
+        public async Task<IEnumerable<Lanche>> GetLanchesReport()
+        {
+            var lanches = await context.Lanches.ToListAsync();
+
+            if (lanches is null)
+                return default(IEnumerable<Lanche>);
+
+            return lanches;
+        }
+
+        public async Task<IEnumerable<Categoria>> GetCategoriaReport()
+        {
+            var categorias = await context.Categorias.ToListAsync();
+
+            if (categorias is null)
+                return default(IEnumerable<Categoria>);
+
+            return categorias;
+        }
+    }
+}
